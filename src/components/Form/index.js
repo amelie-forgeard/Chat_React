8a1c5fb6d205b3ddc15c86f7a0809639ab93205d
vincentferniotoclock <vincent-ferniot@oclock.io>
2022-03-08 14:54:20 +0100

    mise en place du scroll auto sur les messages

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addMessage, changeInputValue } from 'src/actions';
 import './style.scss';
--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import Message from 'src/components/Message';
 import './style.scss';
@@ -5,14 +6,37 @@ import './style.scss';
 export default function Messages() {
   const messages = useSelector((state) => state.messages);
 
+  // useRef nous permet de mettre une référence sur le composant React
+  // une fois injecté dans le DOM, on se sert de cette référence pour l'atteindre
+  const messagesRef = useRef();
+
+  const scrollRef = useRef();
+
+  // on veut scroller automatiquement en bas de la zone à chaque nouveau message
+  useEffect(() => {
+    // quand le callback de useEffect est exécuté, on est après la phase de commit
+    // on a donc accès au DOM
+    // 1e méthode en impératif
+    // const messagesElement = document.querySelector('.messages');
+    // messagesElement.scroll(0, messagesElement.scrollHeight);
+
+    // 2e méthode
+    // messagesRef.current.scroll(0, messagesRef.current.scrollHeight);
+
+    // 3e méthode
+    scrollRef.current.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
+
   return (
-    <div className="messages">
+    <div className="messages" ref={messagesRef}>
       {messages.map((message) => (
         <Message
           key={message.id}
           {...message}
         />
       ))}
+      {/* on place une div en bas de tous les messages */}
+      <div ref={scrollRef} />
     </div>
   );
 }
