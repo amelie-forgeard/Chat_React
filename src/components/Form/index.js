a9c1767e1b80edc8a72e4d268a4c815ebe9e0a58
vincentferniotoclock <vincent-ferniot@oclock.io>
2022-03-08 11:18:33 +0100

    dispatch de base pour l'action ADD_MESSAGE

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,8 +1,25 @@
+import { useDispatch } from 'react-redux';
 import './style.scss';
 
 export default function Form() {
+  // on récupère la méthode dispatch du store avec le hook useDispatch
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // ici je veux changer le tableau de messages
+    // qui est dans le state
+    // le state est dans le store, il faut communiquer
+    // avec ce store => dispatch !!
+    console.log('je soumets le form');
+
+    dispatch({
+      type: 'ADD_MESSAGE',
+    });
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <input
         className="form__input"
         type="text"
