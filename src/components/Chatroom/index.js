fccf49f10911851d3637ba496f444dd17fed5741
vincentferniotoclock <vincent-ferniot@oclock.io>
2022-03-08 09:28:05 +0100

    découpe statique

--- src/components/Chatroom/index.js
+++ src/components/Chatroom/index.js
@@ -2,6 +2,13 @@ import './style.scss';
 
 export default function Chatroom() {
   return (
-    <div className="chatroom">Chatroom</div>
+    <div className="chatroom">
+      <div>Messages
+        <p>Message</p>
+        <p>Message</p>
+        <p>Message</p>
+      </div>
+      <div>Form</div>
+    </div>
   );
 }
