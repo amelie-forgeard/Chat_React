d0132766b5a962bf1ddad18b1fa4851e608992cf
vincentferniotoclock <vincent-ferniot@oclock.io>
2022-03-11 10:50:02 +0100

    utilistion de classNames pour générer les noms de classe

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,13 +1,18 @@
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { isMessageMine } from 'src/selectors';
+import classNames from 'classnames';
 import './style.scss';
 
 export default function Message({ author, content }) {
   // on détermine si le message est le nôtre en comparant l'author
   // avec le user connecté à l'application (pseudo)
   const isMine = useSelector(isMessageMine(author));
-  const classnames = isMine ? 'message message--is-mine' : 'message';
+
+  // const classnames = isMine ? 'message message--is-mine' : 'message';
+  // classNames est une librairie utilitaire pour déterminer si une classe
+  // doit être présente ou non dans notre jsx
+  const classnames = classNames('message', { 'message--is-mine': isMine });
 
   return (
     <div className={classnames}>
