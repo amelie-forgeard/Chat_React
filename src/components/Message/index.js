bcfe0a199f1058c3b8d70c427eb49970a75b2b33
vincentferniotoclock <vincent-ferniot@oclock.io>
2022-03-11 10:18:12 +0100

    injection de la classe CSS message--is-mine si l'auteur et le pseudo sont égaux

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import './style.scss';
 
 export default function Message({ author, content }) {
   // on détermine si le message est le nôtre en comparant l'author
   // avec le user connecté à l'application (pseudo)
+  const pseudo = useSelector((state) => state.user.pseudo);
+  const classnames = author === pseudo ? 'message message--is-mine' : 'message';
 
   return (
-    <div className="message message--is-mine">
+    <div className={classnames}>
       <p className="message__author">{author}</p>
       <p className="message__content">{content}</p>
     </div>
